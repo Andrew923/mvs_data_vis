9e5a326c2cb2d14281a00ef6b7a4de304ea56596
Andrew923 <yuandrew923@gmail.com>
2023-05-09 16:21:56 -0400

    gcp backend

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Upload from "./components/Upload.js";
 import SendData from "./utils/sendData.js";
 
 // export const backend = "http://localhost:9233";
-export const backend = "https://mvs-data-vis.fly.dev/";
+export const backend = "http://34.145.175.29";
 
 const titleStyles = {
   "fontFamily": "monaco, monospace",
